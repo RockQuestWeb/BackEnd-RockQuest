b828f584f8c889d94532448c5b3bffeb1932c19f
NicolasSoler <josolerh@poligran.edu.co>
2020-11-18 15:39:39 -0500

    Correccion de la ruta

--- router/user.js
+++ router/user.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const userController = require('../controller/user.controller');
 var router = express.Router();
 const UserController = require('../controller/user.controller');
 
 router.get('/', UserController.getAll);
 router.get('/:userId', UserController.getOneById);
 router.post('/', UserController.post);
+router.get('mail/:mail', userController.getOneByMail);
 
 module.exports = router;
